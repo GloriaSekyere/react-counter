9019c9ff07d5cd6459184f7795c0a0cccd53fd4d
GloriaSekyere <gloriasekyere15@gmail.com>
2022-02-28 16:28:01 +0000

    use anony,ous functions in onclick instead

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ function App() {
 
   const [count, setCount] = useState(0)
 
-  const handleAdd = () => {
-    setCount(prevCount => prevCount + 1)
-  }
-
-  const handleSubtract = () => {
-    setCount(prevCount => prevCount - 1)
-  }
-
-  const handleReset = () => {
-    setCount(0)
-  }
-
   return (
     <div className="App">
+
       <p className='count'>{count}</p>
+
       <div className='btns'>
-        <button className='add-btn' onClick={handleAdd}>+</button>
-        <button className='sub-btn' onClick={handleSubtract}>-</button>
-        <button className='res-btn' onClick={handleReset}>Reset</button>
+        <button 
+          className='add-btn' 
+          onClick={() => setCount(prevCount => prevCount + 1)}>
+            +
+        </button>
+
+        <button 
+          className='sub-btn' 
+          onClick={() => setCount(prevCount => prevCount - 1)}>
+            -
+        </button>
+
+        <button 
+          className='res-btn' 
+          onClick={() => setCount(0)}>
+            Reset
+        </button>
       </div>
+      
     </div>
   );
 }
